fc54b16c7a7c8cb0438a0197cf6be8150039c5f3
Florian5769 <florianspedalotto@yahoo.fr>
2023-07-06 09:30:10 +0200

    aaa

---
+++ TodoApi/Model/TodoContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
+},
+
+namespace TodoApi.Models;
+
+public class TodoItemDTO
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+namespace TodoApi.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
